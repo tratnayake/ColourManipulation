9be96c2c99f455e7a9ad04a03b3a710c7e7a9f1b
JustinTom <cyclostofu@hotmail.com>
2015-09-17 11:45:14 -0700

    Added Negative manipulation
    Cleaned up code, removed some debugging code, added negative
    manipulation option, commented out while loop. Program closes after
    each manipulation - requires program to be re-ran for another image
    filter.

--- src/pkg7615_assignment_1/Main.java
+++ src/pkg7615_assignment_1/Main.java
@@ -20,15 +20,16 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
        
             // TODO code application logic here
             System.out.println("Program Start");
             
-            boolean loopCond = true;
+            //boolean loopCond = true;
                 
             //Loop through each option.
-                while (loopCond){
+                //while (loopCond){
                 System.out.println("Pick a file");
                 //1. Pick A File
                 String saveDir = FileChooser.getMediaDirectory();
@@ -45,8 +46,9 @@ public class Main {
 
                 Scanner scan = new Scanner(System.in);
                 String userInput = scan.nextLine();
-
-                System.out.println(userInput);
+                
+                //Prints out the user's chosen option - debugging.
+                //System.out.println(userInput);
 
                 switch(userInput){
                     case "1":
@@ -55,13 +57,16 @@ public class Main {
                         break;
                     case "2":
                         System.out.println("Greyscale chosen!");
+                        greyscale(pictureObj);
                         break;
                     case "3":
-                        System.out.println("Option 3 chosen!");
+                        System.out.println("Negative chosen!");
                         negative(pictureObj);
                         break;
                 }
-            }      
+                //Close the program.
+                System.exit(0);
+            //}      
     }
     
     
@@ -73,12 +78,54 @@ public class Main {
         System.out.println("Hit any key to continue adding filter to picture");
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-        increaseColour("Red",pictureObj,3);
+        
+        increaseColour("Red",pictureObj,1.2);
+        decreaseColour("Green",pictureObj,1.2);
+        decreaseColour("Blue",pictureObj,1.2);
+        
+        //Repaint the picture object
+        pictureObj.repaint();
+        
+        System.out.println("Image manipulation complete, press any key to close the program.");
+        Scanner scan2 = new Scanner(System.in);
+        String s2 = scan2.nextLine();
+        //Show the final filtered image.
         pictureObj.show();
     }
     
-    //JUSTIN: Add your method in here pl0x.
+    //Method to turn a (colored) image to greyscale
     private static void greyscale(Picture pictureObj){
+        //Show the original picture
+        pictureObj.show();
+        //Wait for input to continue
+        System.out.println("Hit any key to continue adding filter to picture");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        
+        Pixel[] pixelArray = pictureObj.getPixels();
+        
+        for(Pixel pixelObj : pixelArray){
+            //Get initial values
+            int redValue = pixelObj.getRed();
+            int greenValue = pixelObj.getGreen();
+            int blueValue = pixelObj.getBlue();
+            
+            //Alter the RGB values of the pixel in order to process it to a greyscale.
+            //0.299 x R / 0.587 x G / 0.114 x B algorithm for greyscale and human perception.
+            int grey = (int) (0.299 * redValue + 0.587 * greenValue + 0.114 * blueValue);
+            
+            //Assigns the new RGB pixel values
+            pixelObj.setRed(grey);
+            pixelObj.setGreen(grey);
+            pixelObj.setBlue(grey);
+        }
+        //Updates the newly manipulated pixels for the image.
+        pictureObj.repaint();
+        System.out.println("Image manipulation complete, press any key to close the program.");
+        Scanner scan2 = new Scanner(System.in);
+        String s2 = scan2.nextLine();
+        //Display all the changes made to the photo
+        pictureObj.show();
         
     }
     
@@ -102,16 +149,15 @@ public class Main {
             pixelObj.setRed(getInverse(redValue));
             pixelObj.setGreen(getInverse(greenValue));
             pixelObj.setBlue(getInverse(blueValue));
-            
-            
-            //Just for debugging
-            pictureObj.repaint();
-            
-            
-            
-           
+       
         }
-        
+        //Update all the pixels in the newly fileterd image.
+        pictureObj.repaint();
+        System.out.println("Image manipulation complete, press any key to close the program.");
+        Scanner scan2 = new Scanner(System.in);
+        String s2 = scan2.nextLine();
+        //Show the final filtered image.
+        pictureObj.show();
     }
     
     public static int getInverse(int colorValue){
@@ -177,8 +223,60 @@ public class Main {
                 //Set the pixel;    
                 pixelObj.setBlue(colourValue);
             }
-        }
-        //Repaint the image with new values.
-        pictureObj.repaint();  
+        } 
     }   
+    //Supporting method that does all the increasing of a specific colour.
+    private static void decreaseColour(String colour, Picture pictureObj, double amount){
+      
+        Pixel[] pixelArray = pictureObj.getPixels();
+        
+        for (Pixel pixelObj : pixelArray){
+            //Red
+            if (colour.equals("Red")){
+                int colourValue = pixelObj.getRed();
+                //System.out.println("Initial red Value is " + redValue);
+                //Increment the value of the pixel by 100;
+                colourValue /= amount;
+                //Check the value, if it's more than 255 max it.
+                if (colourValue < 0) {
+                    colourValue = 0;
+                }
+                //System.out.println("New Red Value is " + redValue);
+                //Set the pixel;    
+                pixelObj.setRed(colourValue);
+            }
+            
+            //Green
+            if (colour.equals("Green")) {
+                int colourValue = pixelObj.getGreen();
+                //System.out.println("Initial red Value is " + redValue);
+                //Increment the value of the pixel by 100;
+                colourValue /= amount;
+                //Check the value, if it's more than 255 max it.
+                if (colourValue < 0) {
+                    colourValue = 0;
+                }
+                //System.out.println("New Red Value is " + redValue);
+                //Set the pixel;    
+                pixelObj.setGreen(colourValue);
+            }
+            
+            //Blue
+            if (colour.equals("Blue")) {
+                int colourValue = pixelObj.getBlue();
+
+                //System.out.println("Initial red Value is " + redValue);
+                //Increment the value of the pixel by 100;
+                colourValue /= amount;
+                //Check the value, if it's more than 255 max it.
+                if (colourValue < 0) {
+                    colourValue = 0;
+                }
+                //System.out.println("New Red Value is " + redValue);
+                //Set the pixel;    
+                pixelObj.setBlue(colourValue);
+            }
+        }
+    }  
+    
 }
